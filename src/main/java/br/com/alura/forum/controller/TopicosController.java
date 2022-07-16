af12b54ae9c8f65a882a3d052edb1a7ed2935c01
Danilo Massoni <massonidanilo@gmail.com>
2022-07-16 18:36:02 -0300

    Inserindo o listar e o cadastrar

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,10 +8,14 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.Entity;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -38,9 +42,12 @@ public class TopicosController {
     }
 
     @PostMapping
-    public void cadastrar(@RequestBody TopicoForm form) {
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
             Topico topico = form.converter(cursoRepository);
             topicoRepository.save(topico);
+
+            URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+            return ResponseEntity.created(uri).body(new TopicoDto(topico));
         }
 
 
